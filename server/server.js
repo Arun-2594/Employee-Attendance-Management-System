6ac93bbbd88823649affae0d20f2f0b5c6f53c06
Arun Ananth <arun2594tinku@gmail.com>
2025-12-23 16:02:55 +0530

    Fix App.js location for Vercel build

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const app = express();
 const allowedOrigins = [
   'http://localhost:3000',
   'http://127.0.0.1:3000',
-  'https://employee-attendance-management-system.vercel.app',
+  'https://employee-attendance-management-syst-ashy.vercel.app/',
   'https://employee-attendance-management-system-7muj.vercel.app'
 ];
 
